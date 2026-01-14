6db438dff9d6f852f86de91b60e35d563c8102a8
qeewpi <leyashml@gmail.com>
2026-01-15 05:02:07 +0800

    Controlling bean selection

--- src/main/java/com/asrmanalo/store/PayPalPaymentService.java
+++ src/main/java/com/asrmanalo/store/PayPalPaymentService.java
@@ -2,7 +2,7 @@ package com.asrmanalo.store;
 
 import org.springframework.stereotype.Service;
 
-@Service
+@Service("paypal")
 public class PayPalPaymentService implements PaymentService {
 
     @Override
