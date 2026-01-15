3aa2214b82edc08996c3094ff9d49aa02e939cc0
qeewpi <leyashml@gmail.com>
2026-01-15 10:38:27 +0800

    Simplifying code with Lombok

--- src/main/java/com/asrmanalo/store/StoreApplication.java
+++ src/main/java/com/asrmanalo/store/StoreApplication.java
@@ -1,5 +1,6 @@
 package com.asrmanalo.store;
 
+import com.asrmanalo.store.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,7 +10,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class StoreApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+//        ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+        var user = User.builder()
+                    .id(1L)
+                    .name("name")
+                    .password("password")
+                    .email("email")
+                    .build();
     }
 
 }
--- src/main/java/com/asrmanalo/store/entities/User.java
+++ src/main/java/com/asrmanalo/store/entities/User.java
@@ -1,7 +1,13 @@
 package com.asrmanalo.store.entities;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "users")
 public class User {
@@ -18,35 +24,4 @@ public class User {
     @Column(nullable = false, name = "password")
     private String password;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
